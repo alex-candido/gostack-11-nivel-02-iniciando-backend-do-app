37380f408248a2270494e8edf605a4dbfab73fc2
Alex Cândido <alex.candido.tec@gmail.com>
2022-08-15 11:28:29 -0300

    refatorado appointments.routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,12 @@
 import { parseISO } from 'date-fns';
 import { Router } from 'express';
-
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import { appointmentsRepository } from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-// Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
-
-// SoC: Separations of concerns (Separação de preocupações)
 appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+  const appointments = appointmentsRepository.find();
 
   return response.json(appointments);
 });
@@ -22,9 +17,7 @@ appointmentsRouter.post('/', (request, response) => {
 
     const parsedDate = parseISO(date);
 
-    const CreateAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const CreateAppointment = new CreateAppointmentService();
 
     const appointment = CreateAppointment.execute({
       date: parsedDate,
