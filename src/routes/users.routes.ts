7b60b7f4896478ca38cdf954780ccb15849052e4
Alex Cândido <alex.candido.tec@gmail.com>
2022-08-17 16:26:23 -0300

    metodo create e verificação catch em users.routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,28 @@
 import { Router } from 'express';
+import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
+  try {
+    const { name, email, password } = request.body;
 
-  return response.json({ name });
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (err) {
+    let errorMessage;
+    if (err instanceof Error) {
+      errorMessage = { error: err.message };
+    }
+    return response.status(400).json(errorMessage);
+  }
 });
 
 export default usersRouter;
