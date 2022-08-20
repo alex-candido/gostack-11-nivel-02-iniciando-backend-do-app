89c0c881bf0f51befed806a96637a11ac5cc1eb0
Alex Cândido <alex.candido.tec@gmail.com>
2022-08-20 17:59:02 -0300

    lidando com upload em users.routes.ts

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateRefactoredUser from '../services/CreateRefactoredUser';
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -29,8 +32,14 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-  return response.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
